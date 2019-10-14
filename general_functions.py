1eac0a1dfb634e98b45b21a7c741d0a39e636b6b
nathhje <n.van.sburg@hotmail.nl>
2019-10-14 13:37:01 +0200

    Update general_functions.py

--- general_functions.py
+++ general_functions.py
@@ -14,13 +14,57 @@ import numpy as np
 # # sparse matrix en normaal distribution (vol)
 #
 #
-# def parent_selection():
-#     #tournament
-#     #to do: maak clonen onmogelijk
-#
-# def cross_breeding():
-#     # coinflip
-#     # Rekening houden met welke waardes bias is!
-# 
-# def mutate():
-#     # 1 waarde vervangen (uit normaal distribution)
+def parent_selection(player_number, fitnesses):
+    #tournament
+    #to do: maak clonen onmogelijk
+    
+    parent1 = 0
+    parent2 = 0
+    
+    a = random.randomint(0,player_number-1)
+    b = random.randomint(0,player_number-1)
+    
+    if fitnesses[a] > fitnesses[b]:
+        parent1 = a
+    else:
+        parent1 = b
+        
+    while True:
+        
+        a = random.randomint(0,player_number-1)
+        b = random.randomint(0,player_number-1)
+        
+        if fitnesses[a] > fitnesses[b]:
+            parent2 = a
+        else:
+            parent2 = b
+            
+        if parent1 == parent2:
+            break
+        
+    return parent1,parent2
+
+def cross_breeding(parent1, parent2):
+    # coinflip
+    # Rekening houden met welke waardes bias is!
+    
+    child1 = []
+    child2 = []
+    
+    for i in range(len(parent1)):
+        
+        if random.random() > 0.5:
+            child1.append(parent1[i])
+            child2.append(parent2[i])
+            
+        else:
+            child1.append(parent2[i])
+            child2.append(parent1[i])
+            
+    return child1, child2
+
+def mutate(player, mutation_rate):
+    # 1 waarde vervangen (uit normaal distribution)
+    
+    if random.random() < mutation_rate:
+        player[random.randomint(0,len(player))] = np.random.normal(loc = 0, scale=1)
